320de648a2f67f4c1adc11ce638ca3dc045d682e
broukosklivak <tomas.bartek@creativehill.cz>
2024-08-02 12:52:40 +0200

    Pridani komentaru

--- script.js
+++ script.js
@@ -1,51 +1,58 @@
+// Načítání dat o městech a nastavení našeptávače a odeslání formuláře po načtení stránky
 window.onload = function(){
     getCityList().then(cityList => {
-        const searchTerms = extractSearchTerms(cityList);
-        const sortedSearchTerms = sortSearchTerms(searchTerms);
-        setupAutocomplete(document.getElementById("myInput"), sortedSearchTerms, cityList);
-        setupFormSubmit(document.getElementById("myForm"), document.getElementById("myInput"), cityList, document.getElementById("cityText"));
+        const searchTerms = extractSearchTerms(cityList); // Extrahuje vyhledávací termíny z načteného seznamu měst
+        const sortedSearchTerms = sortSearchTerms(searchTerms); // Seřadí vyhledávací termíny
+        setupAutocomplete(document.getElementById("myInput"), sortedSearchTerms, cityList); // Nastaví funkci našeptávání
+        setupFormSubmit(document.getElementById("myForm"), document.getElementById("myInput"), cityList, document.getElementById("cityText")); // Nastaví odesílání formuláře
     });
 }
 
+// Načítání seznamu měst z externího JSON souboru
 async function getCityList(){
     const response = await fetch("city.list.json");
     const cityList = await response.json();
     return cityList;
 }
 
+// Extrakce názvů měst z načteného seznamu měst a přidání zkratek zemí a států
 function extractSearchTerms(cityList){
     return Object.values(cityList).map(city => {
         return city.state !== "" ? `${city.name}, ${city.country}, ${city.state}` : `${city.name}, ${city.country}`;
     });
 }
 
+// Seřazení vyhledávacích termínů podle abecedy
 function sortSearchTerms(terms){
     return terms.sort()
 }
 
+// Vyhledání města v seznamu měst podle názvu města z našeptávače
 function selectCity(cityList, cityName){
     return Object.values(cityList).find(city => {
         return `${city.name}, ${city.country}, ${city.state}` === cityName || `${city.name}, ${city.country}` === cityName;
     });
 }
 
+// Nastavení našeptávání pro vstupní pole
 function setupAutocomplete(inp, arr, cityList){
     let currentFocus = -1
 
     inp.addEventListener("input", function(){
-        handleInputEvent(inp, arr, cityList, currentFocus);
+        handleInputEvent(inp, arr, cityList, currentFocus); // Zpracuje událost vstupu
     });
     inp.addEventListener("keydown", function(e){
-        handleKeyDown(e, currentFocus);
+        handleKeyDown(e, currentFocus); // Zpracuje událost stisknutí klávesy
     });
     document.addEventListener("click", function(e){
-        handleDocumentClick(e, inp);
+        handleDocumentClick(e, inp); // Zpracuje událost kliknutí
     });
 }
 
+// Zpracování události vstupu pro našeptávání
 function handleInputEvent(inp, arr, cityList, currentFocus){
     const val = inp.value;
-    closeAllLists(inp);
+    closeAllLists(inp); // Zavře všechny seznamy
     if(!val) return false;
 
     const listContainer = document.createElement("DIV");
@@ -62,28 +69,29 @@ function handleInputEvent(inp, arr, cityList, currentFocus){
         item.innerHTML += `<input type='hidden' value='${city}'>`;
         item.addEventListener("click", function(){
             inp.value = this.getElementsByTagName("input")[0].value;
-            closeAllLists(inp);
+            closeAllLists(inp); // Zavře seznam po kliknutí
         });
         listContainer.appendChild(item);
     })
 }
 
+// Zpracování události stisknutí klávesy pro našeptávání
 function handleKeyDown(e, currentFocus){
     const listContainer = document.getElementById(`${e.target.id}-autocomplete-list`);
     if(!listContainer) return;
 
     const items = listContainer.getElementsByTagName("div");
-    if(e.keyCode == 40){
+    if(e.keyCode == 40){ // Klávesa šipky dolů
         e.preventDefault();
         currentFocus++;
         addActive(items, currentFocus);
     } 
-    else if(e.keyCode == 38){
+    else if(e.keyCode == 38){ // Klávesa šipky nahoru
         e.preventDefault();
         currentFocus--;
         addActive(items, currentFocus);
     }
-    else if(e.keyCode == 13){
+    else if(e.keyCode == 13){ // Klávesa Enter
         e.preventDefault();
         if(currentFocus > -1){
             if(items) items[currentFocus].click();
@@ -91,24 +99,28 @@ function handleKeyDown(e, currentFocus){
     }
 }
 
+// Zpracování události kliknutí pro našeptávání
 function handleDocumentClick(e, inp){
-    closeAllLists(inp, e.target)
+    closeAllLists(inp, e.target); // Zavře všechny seznamy při kliknutí mimo
 }
 
+// Přidání třídy aktivního prvku
 function addActive(items, currentFocus){
     if(!items) return;
-    removeActive(items);
+    removeActive(items); // Odstraní třídu aktivního prvku
     if(currentFocus >= items.length) currentFocus = 0;
     if(currentFocus < 0) currentFocus = (items.length - 1);
     items[currentFocus].classList.add("autocomplete-active");
 }
 
+// Odstranění třídy aktivního prvku
 function removeActive(items){
     for(const item of items){
         item.classList.remove("autocomplete-active");
     }
 }
 
+// Zavření všech seznamů našeptávání
 function closeAllLists(inp, elmnt){
     var items = document.getElementsByClassName("autocomplete-items");
     for(const item of items){
@@ -118,14 +130,16 @@ function closeAllLists(inp, elmnt){
     }
 }
 
+
+// Nastavení odesílání formuláře a zpracování počasí pro vybrané město
 function setupFormSubmit(form, inp, cityList, cityText){
     form.addEventListener("submit", function(e){
         e.preventDefault();
-        const selectedCity = selectCity(cityList, inp.value);
+        const selectedCity = selectCity(cityList, inp.value); // Najde vybrané město
         if(selectedCity){
             getWeatherData(selectedCity)
                 .then(weatherData => {
-                    createTable(weatherData);
+                    createTable(weatherData); // Vytvoří tabulku s počasím
                     cityText.innerHTML = inp.value;
                 })
                 .catch(error => console.error('Error:', error))
@@ -133,8 +147,10 @@ function setupFormSubmit(form, inp, cityList, cityText){
     });
 }
 
+
+// Získání dat o počasí pro vybrané město
 async function getWeatherData(cityData){
-    const unit = navigator.language === "en-US" ? "imperial" : "metric";
+    const unit = navigator.language === "en-US" ? "imperial" : "metric"; // Nastaví jednotky podle jazyka
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityData.coord.lat}&lon=${cityData.coord.lon}&units=${unit}&appid=3097a1417bbafbd6e40a98e639e9d104`;
     const response = await fetch(apiUrl);
     if(!response.ok){
@@ -144,6 +160,7 @@ async function getWeatherData(cityData){
     return data;
 }
 
+// Vytvoření tabulky s předpovědí počasí
 function createTable(weatherData){
     const table = document.getElementById("myTable");
     const lang = navigator.language;
@@ -176,6 +193,7 @@ function createTable(weatherData){
     table.innerHTML = tableContent;
 }
 
+// Formátování času podle jazyka
 function formatTime(timePart, lang){
     const timeArr = timePart.split(":");
     const hour = parseInt(timeArr[0], 10);
@@ -212,6 +230,7 @@ function formatTime(timePart, lang){
     return { displayTime, unit };
 }
 
+// Formátování data podle jazyka
 function formatDate(currentStampDate, lang) {
     const currentDateArr = currentStampDate.split("-");
 
